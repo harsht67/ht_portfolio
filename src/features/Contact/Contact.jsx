2ac5e9f6f5f95166add6a0f334b85c7fd1364107
Harsh Tomar <harshtomar67tb@gmail.com>
2022-06-15 21:50:47 +0530

    feat: Add Contact

--- src/features/Contact/Contact.jsx
+++ src/features/Contact/Contact.jsx
@@ -1,11 +1,79 @@
 // styles
 import './Contact.scss'
 
+// icons
+import { TiSocialGithubCircular, TiSocialLinkedinCircular, TiSocialAtCircular } from 'react-icons/ti'
+import { RiPhoneFill } from 'react-icons/ri'
+
 function Contact() {
     return (
         <div className="contact">
 
-            contact desu
+            <h1 className="contact__title">
+                Contact 
+            </h1>
+
+            <div className="contact__content">
+
+                <section className="contact__form">
+
+                    <label>
+                        
+                        Name 
+                        
+                        <input
+                            placeholder='harsh tomar'
+                        />
+
+                    </label>
+
+                    <label>
+                        
+                        Email
+                        
+                        <input
+                            placeholder='harsh@gmail.com'
+                        />
+
+                    </label>
+
+                    <label>
+                        
+                        Message
+                        
+                        <textarea
+                            placeholder='lorem ispum...'
+                        />
+
+                    </label>
+
+                    <button>
+                        send
+                    </button>
+
+                </section>
+
+                <aside className="contact__links">
+
+                    <p>
+                        <RiPhoneFill/> 9582594496
+                    </p>
+
+                    <p>
+                        <TiSocialAtCircular/> harshtomar@gmail.com
+                    </p>
+
+                    <p>
+                        <TiSocialGithubCircular/> github.harshtomar.com
+                    </p>
+                    
+                    <p>
+                        <TiSocialLinkedinCircular/> linkedin.harshtomar.com
+                    </p>
+
+                </aside>
+
+            </div>
 
         </div>
     )
