b62cdb61190a757d813920c00d34e6fd1a4d8d42
Harsh Tomar <harshtomar67tb@gmail.com>
2022-06-17 14:50:33 +0530

    feat: Add filter Works component filter by type (all, frontend, fullstack)

--- src/features/Works/Works.jsx
+++ src/features/Works/Works.jsx
@@ -9,20 +9,47 @@ import { useState, useEffect } from 'react'
 import { client, urlFor } from '../../client'
 
 const filters = ['all', 'frontend', 'fullstack']
-// const projects = ['portfolio website', 'amazon clone', 'invoice app', 'todo app', 'map API']
 
 function Works() {
 
     const [works, setWorks] = useState([])
 
+    const [filterWorks, setFilterWorks] = useState([])
+
+    const [currFilter, setCurrFilter] = useState('all')
+
+    // fetch all works
     useEffect(() => {
         const query = '*[_type == "works"]'
 
         client
             .fetch(query)
-            .then(data => setWorks(data))
+            .then(data => {
+                setWorks(data)
+                setFilterWorks(data)
+            })
             
     }, [])
+
+    // filters work wrt to current filter 
+    const filterFunc = (filter) => {
+        console.log('>>>>>>'+filter)
+
+        if(filter=='all') {
+            setFilterWorks(works)
+        }
+        else {
+            setFilterWorks(works.filter(work => work.type==filter))
+        }
+
+    }
+
+    // changes current filter
+    const changeFilter = (filter) => {
+        setCurrFilter(filter)
+
+        filterFunc(filter)
+    }
 
     return (
         <div className="works">
@@ -33,7 +60,11 @@ function Works() {
 
             <ul className="works__filters">
                 { filters.map(filter => (
-                    <li key={filter}>
+                    <li 
+                        key={filter}
+                        className={currFilter==filter && 'active'}
+                        onClick={() => changeFilter(filter)}
+                    >
                         {filter}
                     </li>
                 )) }
@@ -41,7 +72,7 @@ function Works() {
 
             <section className="works__content">
 
-                { works.map(work => <Box data={work} /> )}
+                { filterWorks.map(work => <Box data={work} /> )}
 
             </section>
 
