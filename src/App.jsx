1c6c950feb4370c7a037c4eb7139ea25f212deb8
Harsh Tomar <harshtomar67tb@gmail.com>
2022-06-21 12:39:48 +0530

    feat: Update theme user preference browser preference

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,19 @@ import { Header, Footer, Home, About, Works, Work, Contact } from './features'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { useState } from 'react'
+import useLocalStorage from 'use-local-storage'
+import { useEffect } from 'react'
 
 function App() {
 
-    const [theme, setTheme] = useState('light')
+    const isDefaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+
+    const [theme, setTheme] = useLocalStorage('theme', isDefaultDark ? 'dark' : 'light')
 
     const toggleTheme = () => {
         setTheme(prev => prev=='light'?'dark':'light')
     }
-
+    
     return (
         <div className={`app theme-${theme}`}>
 
